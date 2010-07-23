edfa09f52f6574e82a8ae86cb44306ad501bc816
Dean Landolt <dean@deanlandolt.com>
2010-07-23 15:49:39 -0700

    add createWriteStream to rhino fs

--- engines/rhino/lib/fs.js
+++ engines/rhino/lib/fs.js
@@ -1,4 +1,5 @@
-var File = require("file");
+var File = require("file"),
+    defer = require("./promise").defer;
 exports.readFileSync = exports.read = File.read;
 exports.writeFileSync = exports.write = File.write;
 exports.statSync = File.stat;
@@ -7,3 +8,27 @@ exports.makeTree = File.mkdirs;
 exports.makeDirectory = File.mkdir;
 
 exports.open = File.open;
+
+exports.createWriteStream = function(path, options) {
+    options = options || {};
+    options.flags = options.flags || "w";
+    var flags = options.flags || "w",
+        f = File.open(path, flags);
+    return {
+        writable: true,
+        write: function() {
+            var deferred = defer();
+            try {
+                f.write.apply(this, arguments);
+                f.flush();
+            }
+            catch (e) {
+                return stream.writable = false;
+            }
+            deferred.resolve();
+            return deferred.promise;
+        },
+        end: f.close,
+        destroy: f.close
+    }
+}
