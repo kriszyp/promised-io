498ac694aac7222930847f13e343473f220c5092
Mark Wubben <mark@novemberborn.net>
2010-12-08 12:40:17 +0100

    Add a test case for a nested whenPromise that is missing a rejectCallback. This causes an uncaught exception to be thrown because the error isn't handled on the whenPromise itself, but it is handled later as it bubbles through the promises.

--- tests/promise.js
+++ tests/promise.js
@@ -1,5 +1,6 @@
 var assert = require("assert"),
 	when = require("../lib/promise").when,
+	whenPromise = require("../lib/promise").whenPromise,
 	defer = require("../lib/promise").defer;
 
 exports.testSpeedPlainValue = function(){
@@ -16,6 +17,19 @@ exports.testSpeedPromise = function(){
 		});
 	}
 	deferred.resolve(3);
+};
+
+exports.testWhenPromiseRejectHandled = function(){
+	// The inner whenPromise doesn't have a rejectCallback, but the outer one does.
+	// This means the error then *is* handled, but the inner whenPromise doesn't know about that.
+	// This shouldn't result in an uncaught exception thrown by the promise library.
+	whenPromise(true, function(){
+		return whenPromise((function(){
+			var deferred = defer();
+			deferred.reject({});
+			return deferred.promise;
+		})());
+	}).then(null, function(){});
 };
 
 if (require.main === module)
