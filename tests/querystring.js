1ace4c05edaf40bb25108a81f1628bca5ef295b2
Dean Landolt <dlandolt@sitepen.com>
2014-12-22 17:01:28 -0500

    Clean up test running, remove vows dep

--- tests/querystring.js
+++ tests/querystring.js
@@ -1,30 +1,26 @@
-var vows = require("vows"),
-		assert = require("assert"),
-		querystring = require("../util/querystring");
+var assert = require("assert");
+var querystring = require("../util/querystring");
 
-vows.describe("util/querystring").addBatch({
-	"When munging array parameters": {
-		topic: ["bar", "baz"],
-		"Each array value should have its own parameter name": function(arr){
-			var requestParams = [];
-			querystring.addToArray(requestParams, "foo", arr);
-			assert.deepEqual(requestParams, ["foo[]", "bar", "foo[]", "baz"]);
-		}
-	},
-	"When munging object parameters": {
-		topic: { "key": "value" },
-		"Each property value should have its own parameter name": function(obj){
-			var requestParams = [];
-			querystring.addToArray(requestParams, "obj", obj);
-			assert.deepEqual(requestParams, ["obj[key]", "value"]);
-		}
-	},
-	"When parsing a munged query string": {
-		topic: "foo[]=bar&foo[]=baz&obj[key]=value",
-		"Arrays and objects should retain their own parameter names": function(qs){
-			var requestParams = [];
-			querystring.parseToArray(requestParams, qs);
-			assert.deepEqual(requestParams, ["foo[]", "bar", "foo[]", "baz", "obj[key]", "value"]);
-		}
-	}
-})["export"](module);
+exports.testMungingArrayParams = function(){
+	var arr = ["bar", "baz"];
+	var requestParams = [];
+	querystring.addToArray(requestParams, "foo", arr);
+	assert.deepEqual(requestParams, ["foo[]", "bar", "foo[]", "baz"]);
+};
+
+exports.testMungingObjectParams = function(){
+	var obj = { "key": "value" };
+	var requestParams = [];
+	querystring.addToArray(requestParams, "obj", obj);
+	assert.deepEqual(requestParams, ["obj[key]", "value"]);
+};
+
+exports.testParsingMungedQuerystring = function(){
+	var qs = "foo[]=bar&foo[]=baz&obj[key]=value";
+	var requestParams = [];
+	querystring.parseToArray(requestParams, qs);
+	assert.deepEqual(requestParams, ["foo[]", "bar", "foo[]", "baz", "obj[key]", "value"]);
+};
+
+if (require.main === module)
+    require("patr/runner").run(exports);
