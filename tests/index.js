4f8caaebab96859e2f5a67a223d61ff240903034
Dean Landolt <dlandolt@sitepen.com>
2014-12-22 23:08:11 -0500

    Add promise#spawn coroutine support

--- tests/index.js
+++ tests/index.js
@@ -2,5 +2,12 @@ exports.testOAuth = require('./oauth');
 exports.testPromise = require('./promise');
 exports.testQuerystring = require('./querystring');
 
+// test spawning coroutines if generators are supported on platform
+try {
+	eval("(function* (){})()");
+	exports.testPromiseSpawn = require('./promise-spawn');
+}
+catch (err) {}
+
 if (require.main === module)
 	require("patr/runner").run(exports);
